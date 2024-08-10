147527521f592e79d2b359fe8795e364aeaf0db9
Navdeep434 <n.navdeepraushan98@gmail.com>
2024-08-10 19:58:59 +0530

    typing effect animation added in index page text

---
+++ js/index.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const text = "Explore my journey in\nsoftware engineering...";
+    const typedTextSpan = document.querySelector("#typed-text");
+    const cursorSpan = document.querySelector(".cursor");
+
+    let textIndex = 0;
+    function type() {
+        if (textIndex < text.length) {
+            if (!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+            typedTextSpan.innerHTML = text.substring(0, textIndex + 1).replace('\n', '<br>');
+            textIndex++;
+            setTimeout(type, 100); // Adjust typing speed here
+        } else {
+            cursorSpan.classList.remove("typing");
+        }
+    }
+
+    // Start the typing effect
+    setTimeout(type, 1000); // Delay before typing starts
+});
